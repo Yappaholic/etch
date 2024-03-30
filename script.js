10dca1b07e008c6e1e27ce93447aa2ee34093ba3
Yappaholic <sav.boyar@gmail.com>
2024-03-30 11:38:10 +0300

    Working hover and grid, make flexible

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const container = document.getElementById("container");
-
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
+function createGrid(x) {
+ for (let i = 0; i < x; i++) {
+  for (let j = 0; j < x; j++) {
     const cell = document.createElement("div");
     cell.classList.toggle("cell");
     container.appendChild(cell);
-  }
-}
+  };
+}; 
+};
+createGrid(16)
+const box = document.querySelectorAll(".cell");
+for (let i = 0; i < box.length; i++) {
+ box[i].addEventListener("mouseover", () => {
+    box[i].classList.remove("cell");
+    box[i].classList.add("hover");
+  });
+};
+
