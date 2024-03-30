ca0a179744d41d0cf1a77c0a0030908bf176cf00
Yappaholic <sav.boyar@gmail.com>
2024-03-30 10:41:37 +0300

    Add grid layout, title and css fix

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById("container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const cell = document.createElement("div");
+    cell.classList.toggle("cell");
+    container.appendChild(cell);
+  }
+}
