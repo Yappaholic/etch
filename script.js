5942660fa10740c0e02c800154a5fdfba50a9d76
Yappaholic <sav.boyar@gmail.com>
2024-03-30 12:50:02 +0300

    Add flexible grid layout

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const container = document.getElementById("container");
+const width = 700
+const btn = document.querySelector("#btn")
 function createGrid(x) {
  for (let i = 0; i < x; i++) {
   for (let j = 0; j < x; j++) {
     const cell = document.createElement("div");
     cell.classList.toggle("cell");
+    let resolution = width / x  
     container.appendChild(cell);
   };
-}; 
 };
-createGrid(16)
-const box = document.querySelectorAll(".cell");
-for (let i = 0; i < box.length; i++) {
+  hover()
+ };
+function hover() {
+ const box = document.querySelectorAll(".cell");
+ for (let i = 0; i < box.length; i++) {
  box[i].addEventListener("mouseover", () => {
     box[i].classList.remove("cell");
     box[i].classList.add("hover");
   });
 };
+ 
+}
 
+function removeGrid() {
+  const row = document.querySelectorAll("#container > div")
+  for (let i = 0; i < row.length; i++) {
+    row[i].remove()
+  }
+};
+
+btn.addEventListener("click", () => {
+  let ask = prompt("Choose grid size (max 100)")
+  removeGrid()
+  createGrid(ask)
+});
+createGrid(20)
